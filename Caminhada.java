ed7d910803a38cdd650e45e79649779e4dc46379
andrelimaiftm <36212719+andrelimaiftm@users.noreply.github.com>
2022-04-26 15:32:25 -0300

    Add files via upload

---
+++ Caminhada.java
@@ -0,0 +1,7 @@
+public class Caminhada {
+              //int     x
+    void andar(Pessoa pessoa){   
+        System.out.println("O " + pessoa.nome + " esta passeando com " + pessoa.cachorro.nome);
+    }  
+    
+}
